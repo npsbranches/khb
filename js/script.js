3ae76629adbcd8d77289b057d27012076f5da5ae
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-02-19 12:41:09 +0500

    1.3

--- js/script.js
+++ js/script.js
@@ -13,3 +13,14 @@ $('.menu li a').click(function() {
     $('.menu ul').toggleClass('active');
     $('.btn-bars').toggleClass('active');
 })
+
+
+$(".menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href');
+    var top = $(id).offset().top;
+
+    $('body,html').animate({
+        scrollTop: top
+    }, 1500);
+});
